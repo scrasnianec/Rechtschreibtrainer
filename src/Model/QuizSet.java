60e4afd97b2a2651c8b12c73c4b77de541a41c08
samyepik <samuel.steinhart@gmail.com>
2024-12-16 11:09:07 +0100

    set of quizzes

---
+++ src/Model/QuizSet.java
@@ -0,0 +1,44 @@
+package Model;
+
+import java.util.List;
+import java.util.Random;
+
+public class QuizSet {
+
+	private List<QuizQuestion> quizSet;
+	private boolean[] setHistory;
+	private QuizQuestion quizQuestion;
+	private SaveLoadQuizFile saveLoadQuizFile;
+
+	public QuizSet(SaveLoadQuizFile saveLoadQuizFile) {
+		this.saveLoadQuizFile = saveLoadQuizFile;
+		this.quizSet = saveLoadQuizFile.loadQuestions();
+		this.setHistory = new boolean[quizSet.size()];
+	}
+
+	public QuizQuestion getRandomQuestionFromSet() {
+		if (quizSet.isEmpty()) return null;
+		return quizSet.get(new Random().nextInt(quizSet.size()));
+	}
+
+	public QuizQuestion getRandomQuestionFromFile() {
+		List<QuizQuestion> questions = saveLoadQuizFile.loadQuestions();
+		return questions.get(new Random().nextInt(questions.size()));
+	}
+
+	public void addHistoryEntry(boolean entry) {
+		// Example assumes fixed size history array
+		for (int i = setHistory.length - 1; i > 0; i--) {
+			setHistory[i] = setHistory[i - 1];
+		}
+		setHistory[0] = entry;
+	}
+
+	public int calculatePointsEarned() {
+		int points = 0;
+		for (boolean entry : setHistory) {
+			if (entry) points += 10;
+		}
+		return points;
+	}
+}
