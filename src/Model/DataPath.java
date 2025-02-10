c7d621d18f13325026853006b58fd0a5c7049e40
samyepik <samuel.steinhart@gmail.com>
2025-02-10 10:09:58 +0100

    quickfix

--- src/Model/DataPath.java
+++ src/Model/DataPath.java
@@ -1,5 +1,5 @@
 package Model;
 
 public class DataPath {
-    public static final String QUIZ_PATH = "question_data.json";
+    public static final String QUIZ_PATH = "data/mock_quiz_questions.json";
 }
